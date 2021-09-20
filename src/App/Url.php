177d24ba0c85ffce473e461aef7f65748752dd49
marcosorozco <marcosorozco.14@gmail.com>
2021-09-20 15:09:35 -0500

    first commit

---
+++ src/App/Url.php
@@ -0,0 +1,114 @@
+<?php
+
+use Illuminate\Container\Container;
+
+class Url
+{
+    /**
+     * @var array
+     */
+    private $arrayValues;
+
+    private static $data = [];
+
+    /**
+     * UrlTO constructor.
+     * @param array $arrayValues
+     */
+    public function __construct(array $arrayValues = [])
+    {
+        $this->arrayValues = $arrayValues;
+    }
+
+    public static function setPrevalecenceData(array $arrayData)
+    {
+        self::$data = $arrayData;
+    }
+
+    public static function generateArray(array $array = [], array $except = [], $removeString = '')
+    {
+        $values = Container::getInstance()->make('request')->only(self::$data);
+        $arrayValues = array_merge($array, $values);
+        $result = [];
+        foreach ($arrayValues as $key=>$value) {
+            if (!in_array($key, $except)) {
+                $result[str_replace($removeString, '', $key)] = $value;
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * @param array $array
+     * @param array $except
+     * @param string $removeString
+     * @return array
+     */
+    public function toArray(array $array = [], array $except = [], $removeString = '')
+    {
+        $arrayValues = array_merge($array, $this->arrayValues);
+        $result = [];
+        foreach ($arrayValues as $key=>$value) {
+            if (!in_array($key, $except)) {
+                $result[str_replace($removeString, '', $key)]=$value;
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * @param array $except
+     * @param string $removeString
+     * @return string
+     */
+    public static function generateInput(array $except = [], $removeString = '')
+    {
+        $arrayValues = Container::getInstance()->make('request')->only(self::$data);
+        $result = '';
+        foreach ($arrayValues as $key=>$value) {
+            if (!in_array($key, $except)) {
+                $result.='<input type="hidden" name="'
+                    .str_replace($removeString, '', $key)
+                    .'" value="'.$value.'"/>';
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * @param array $except
+     * @param string $removeString
+     * @return string
+     */
+    public function toInput(array $except = [], $removeString = '')
+    {
+        $arrayValues = $this->arrayValues;
+        $result = '';
+        foreach ($arrayValues as $key=>$value) {
+            if (!in_array($key, $except)) {
+                $result.='<input type="hidden" name="'
+                    .str_replace($removeString, '', $key)
+                    .'" value="'.$value.'"/>';
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     */
+    public function __set($name, $value)
+    {
+        $this->arrayValues[$name] = $value;
+    }
+
+    /**
+     * @param $name
+     * @return mixed|null
+     */
+    public function __get($name)
+    {
+        return $this->arrayValues[$name] ?? null;
+    }
+}
---
+++ src/Utilities/helpers.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Container\Container;
+
+if (! function_exists('route')) {
+
+    /**
+     * Generate the URL to a named route.
+     *
+     * @param  array|string  $name
+     * @param  mixed  $parameters
+     * @param  bool  $absolute
+     * @return string
+     */
+    function route($name, $parameters = [], $absolute = true)
+    {
+        $parameters = Url::generateArray($parameters);
+        return Container::getInstance()
+            ->make('url')
+            ->route($name, $parameters, $absolute);
+    }
+
+    /**
+     * Generate the URL to a named route.
+     *
+     * @param  array|string  $name
+     * @param  mixed  $parameters
+     * @param  bool  $absolute
+     * @return string
+     */
+    function route_prevalence($name, $parameters = [], $absolute = true)
+    {
+        return Container::getInstance()
+            ->make('url', $parameters)
+            ->route($name, $parameters, $absolute);
+    }
+}
